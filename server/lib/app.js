e3901bb0a78087f09e6a319f1d9d7eb8e18928f2
johvik <john.viklund@gmail.com>
2013-11-27 21:59:52 +0100

    Added express package

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,6 +1,8 @@
 var request = require('superagent');
 var async = require('async');
 var cronJob = require('cron').CronJob;
+var express = require('express');
+var app = express();
 var db = require('./db');
 
 // Run every 5 min
@@ -45,5 +47,18 @@ function update() {
   );
 }
 
-// TODO Create server
+// Set up middleware
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.logger('dev'));
+app.use(app.router);
+
+// TODO Set up API
+app.get('/', function(req, res) {
+  res.send('test');
+});
+
+app.listen(9001);
+
 job.start();
